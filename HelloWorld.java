6229b058d7bebb562abcbcd109265f6ced270a74
Eric Cao <ecao310@gmail.com>
2017-11-14 01:41:54 -0500

    sqashing 2 commits into 1

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,6 +1,9 @@
 public class HelloWorld {
   public static void main(String[] args) {
     System.out.println("Hello World!");
-    System.out.println("new change to hello world!");
+    // more random changes
+    // i want to see how this looks in github desktop before i commit
+    // i was able to push through desktop!
+    // didn't even need to to type git push. 
   }
 }
